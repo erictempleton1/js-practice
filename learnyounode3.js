753c73b335e768e0d5f8b047018b5ad7b0b81150
Eric Templeton <erictempleton1@gmail.com>
2015-04-11 15:51:14 -0400

    created function to handle fiile read

--- learnyounode3.js
+++ learnyounode3.js
@@ -1,7 +1,25 @@
 var fs = require('fs')
 
-var reading = fs.readFileSync(process.argv[2]);
+/*
+var reading = fs.readFileSync('learnyounode2.js');
 
 var splits = reading.toString().split('\n');
 
 console.log((splits.length - 1));
+
+console.log(splits);
+
+// check to see what the last element of the list is
+console.log(splits[splits.length - 1]);
+*/
+
+function fileInfo(file_name) {
+    var reading = fs.readFileSync(file_name);
+    var splits = reading.toString().split('\n');
+    if (splits[splits.length - 1] == '')
+        return splits.length - 1;
+    else
+        return splits.length;
+};
+
+console.log(fileInfo('learnyounode2.js'));
