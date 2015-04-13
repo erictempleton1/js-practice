439d741b5e8cf5f220cdf679510304c50c886ba7
Eric Templeton <erictempleton1@gmail.com>
2015-04-13 07:40:00 -0400

    created solution to problem 4

--- learnyounode4.js
+++ learnyounode4.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
 
-fs.readFile('learnyounode3.js', readAsynco);
+fs.readFile(process.argv[2], 'utf8', readAsynco);
 
-function readAsynco(error, data) {
-    // check for error code here
+function readAsynco(error, jsContents) {
+    if (error)
+        return console.error(error);
+    else
+        var splits = jsContents.split('\n');
+        return console.log(splits.length - 1);
+};
