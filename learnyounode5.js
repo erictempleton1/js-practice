d45e763c798c03396717ccaa4ee12c4b60b32d8e
Eric Templeton <erictempleton1@gmail.com>
2015-04-15 07:58:49 -0400

    tesing problem 5

--- learnyounode5.js
+++ learnyounode5.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
-fs.readdir(process.argv[2], folderReader);
+fs.readdir(process.argv[2], process.argv[3], folderReader);
 
 function folderReader(error, dirContents) {
     if (error)
         return console.error(error);
     else
         for (var i = 0; i < dirContents.length; i++) {
-            //if (dirContents[i].slice(-3) == 'jpg')
-            return console.log(dirContents[i]);
+            if (process.argv[2].slice(-process.argv[3].length) == process.argv[3])
+              return console.log(process.argv[2]);
         };
 };
