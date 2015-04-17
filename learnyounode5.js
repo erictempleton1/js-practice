ca5b783580ef8549804e74411ff2247792529cbb
Eric Templeton <erictempleton1@gmail.com>
2015-04-17 06:25:10 -0400

    trying forEach loop instead

--- learnyounode5.js
+++ learnyounode5.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
-fs.readdir(process.argv[2], process.argv[3], folderReader);
+//fs.readdir(process.argv[2], process.argv[3], folderReader);
+
+fs.readdir('.', folderReader);
 
 function folderReader(error, dirContents) {
     if (error)
         return console.error(error);
     else
-        for (var i = 0; i < dirContents.length; i++) {
-            if (dirContents.slice(-process.argv[3].length) == process.argv[3])
-              return console.log(dirContents);
-        };
+        dirContents.forEach(folderReader);
 };
+
