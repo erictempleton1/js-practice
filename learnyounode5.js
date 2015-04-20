d383d8b482efc097902240682d03a44838054221
Eric Templeton <erictempleton1@gmail.com>
2015-04-20 06:46:31 -0400

    final answer for problem 5

--- learnyounode5.js
+++ learnyounode5.js
@@ -2,12 +2,13 @@ var fs = require('fs');
 var path = require('path');
 
 
-fs.readdir('.', function(error, files) {
+fs.readdir(process.argv[2], function(error, files) {
     if (error)
         return console.error(error);
     else
         files.forEach(function(file) {
-            console.log(path.extname(file));
+            if (path.extname(file) == '.' + process.argv[3])
+                console.log(file);
         });
 });
 
